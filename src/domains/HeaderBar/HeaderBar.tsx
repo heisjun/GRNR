2f3b17561d7520668d45d3b81f68535a4d79bb9e
1fuu <jtnnnew@gmail.com>
2022-07-19 17:07:32 +0900

    2207191707

--- src/domains/HeaderBar/HeaderBar.tsx
+++ src/domains/HeaderBar/HeaderBar.tsx
@@ -1,17 +1,41 @@
-import styled from 'styled-components';
+import { useState, useEffect } from 'react';
+import styled, { keyframes } from 'styled-components';
 
 import { IHeaderBar } from './HeaderBar.type';
 
 const HeaderBar: React.FC<IHeaderBar> = (props) => {
     const { children } = props;
+    const [scrollDownToggle, setScrollDownToggle] = useState<boolean>(false);
+    const [currentPosY, setCurentPosY] = useState<number>(0);
+
+    const scrollHandler = () => {
+        setCurentPosY(window.scrollY);
+        if (window.scrollY > currentPosY) {
+            setScrollDownToggle(true);
+        } else {
+            setScrollDownToggle(false);
+        }
+    };
+
+    useEffect(() => {
+        console.log(scrollDownToggle);
+    }, [scrollDownToggle]);
+
+    useEffect(() => {
+        window.addEventListener('scroll', scrollHandler);
+        return () => {
+            window.removeEventListener('scroll', scrollHandler);
+        };
+    }, []);
+
     return (
         <StyledContainer>
-            <StyledHeaderBarContainer>
+            <StyledHeaderBarContainer toggle={scrollDownToggle}>
                 <StyledHeaderBar>
-                    <StyleMenuButton />
-                    <StyleTitleBlock>오늘의집</StyleTitleBlock>
-                    <StyleSearchButton />
-                    <StyleCartButton />
+                    <StyledMenuButton />
+                    <StyledTitleBlock>오늘의집</StyledTitleBlock>
+                    <StyledSearchButton />
+                    <StyledCartButton />
                 </StyledHeaderBar>
             </StyledHeaderBarContainer>
             <StyledContentContainer>{children}</StyledContentContainer>
@@ -19,7 +43,25 @@ const HeaderBar: React.FC<IHeaderBar> = (props) => {
     );
 };
 
-const StyleMenuButton = styled.div`
+const headerFadeIn = keyframes`
+    0% {
+        transform: translateY(-100%);
+    }
+    100% {
+        transform: translateY(0);
+    }
+`;
+
+const headerFadeOut = keyframes`
+    0% {
+        transform: translateY(0%);
+    }
+    100% {
+        transform: translateY(-100%);
+    }
+`;
+
+const StyledMenuButton = styled.div`
     width: 20px;
     height: 20px;
     border-radius: 20px;
@@ -29,7 +71,7 @@ const StyleMenuButton = styled.div`
     }
 `;
 
-const StyleSearchButton = styled.div`
+const StyledSearchButton = styled.div`
     width: 20px;
     height: 20px;
     border-radius: 20px;
@@ -39,7 +81,7 @@ const StyleSearchButton = styled.div`
     }
 `;
 
-const StyleCartButton = styled.div`
+const StyledCartButton = styled.div`
     width: 20px;
     height: 20px;
     border-radius: 20px;
@@ -49,7 +91,7 @@ const StyleCartButton = styled.div`
     }
 `;
 
-const StyleTitleBlock = styled.h1`
+const StyledTitleBlock = styled.h1`
     font-size: 20px;
     @media screen and (max-width: 800px) {
         margin: 0 auto;
@@ -60,7 +102,6 @@ const StyleTitleBlock = styled.h1`
 `;
 
 const StyledContentContainer = styled.div`
-    flex: 1;
     height: 5000px;
     background-color: silver;
     margin-top: 50px;
@@ -69,7 +110,7 @@ const StyledContentContainer = styled.div`
     }
 `;
 
-const StyledHeaderBarContainer = styled.div`
+const StyledHeaderBarContainer = styled.div<{ toggle: boolean }>`
     position: fixed;
     top: 0px;
     display: flex;
@@ -80,6 +121,7 @@ const StyledHeaderBarContainer = styled.div`
     background-color: white;
     border-bottom: solid 1px;
     border-color: #eaeaea;
+    animation: ${headerFadeIn} 0.5s;
 `;
 
 const StyledHeaderBar = styled.div`
@@ -99,6 +141,7 @@ const StyledHeaderBar = styled.div`
 `;
 const StyledContainer = styled.div`
     display: flex;
+    flex-direction: column;
     width: 100%;
 `;
 
