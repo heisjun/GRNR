360ef638acae4bb10d3d6c1750e5258a7176542e
taemin <jtnnnew@gmail.com>
2022-09-20 03:05:27 +0900

    2209200305

--- src/pages/Writing/Question/Question.tsx
+++ src/pages/Writing/Question/Question.tsx
@@ -1,18 +1,60 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useRef, useCallback } from 'react';
 import styled from 'styled-components';
 
 const Question: React.FC = () => {
     const [title, setTitle] = useState<string>('');
     const [content, setContent] = useState<string>('');
+    const [crntRange, setCrntRange] = useState<Range>();
+    const [imgFile, setImgFile] = useState<File>();
 
-    const onContentChange = () => {
-        const divC = document.getElementById('contentInput');
-        setContent(divC ? divC.innerHTML : '');
-    };
+    const imgInputRef = useRef<HTMLInputElement | null>(null);
 
     useEffect(() => {
-        console.log(content);
-    }, [content]);
+        window.addEventListener('selectstart', selectionHandler);
+        return () => {
+            window.removeEventListener('selectstart', selectionHandler);
+        };
+    }, []);
+
+    const selectionHandler = useCallback(() => {
+        setCrntRange(window.getSelection()?.getRangeAt(0));
+    }, []);
+
+    const onContentChange = useCallback(() => {
+        const divC = document.getElementById('contentInput');
+        setContent(divC ? divC.innerHTML : '');
+    }, []);
+
+    const onImgUploadButton = useCallback(() => {
+        if (!imgInputRef.current) {
+            return;
+        }
+        imgInputRef.current.click();
+    }, []);
+
+    const onImgChange = useCallback(
+        (e: React.ChangeEvent<HTMLInputElement>) => {
+            if (e.target.files) {
+                const img = document.createElement('img');
+                const div = document.createElement('div');
+                const reader = new FileReader();
+                reader.readAsDataURL(e.target.files[0]);
+                reader.onload = () => {
+                    if (reader.result) {
+                        img.src = reader.result.toString();
+                    }
+                };
+
+                setImgFile(e.target.files[0]);
+
+                div.appendChild(img);
+                crntRange?.insertNode(div);
+
+                e.target.value = '';
+            }
+        },
+        [crntRange],
+    );
 
     return (
         <StyledQuestionContainer>
@@ -26,10 +68,28 @@ const Question: React.FC = () => {
                 style={title.length === 0 ? { borderColor: 'red' } : { borderColor: 'silver' }}
             />
             {title === '' ? <StyledTitleInfo>필수 입력 항목입니다.</StyledTitleInfo> : null}
-            <StyledInputBlock id="contentInput" contentEditable="true" onInput={onContentChange}></StyledInputBlock>
+
+            <input type="file" accept="image/*" ref={imgInputRef} style={{ display: 'none' }} onChange={onImgChange} />
+            <StyledImgUpload onClick={onImgUploadButton}>img</StyledImgUpload>
+
+            <StyledInputBlock id="contentInput" contentEditable="true" onInput={onContentChange} />
         </StyledQuestionContainer>
     );
 };
+
+const StyledImgUpload = styled.div`
+    width: 50px;
+    height: 20px;
+    background-color: silver;
+    border-radius: 5px;
+    cursor: pointer;
+    font-size: 12px;
+    font-weight: bold;
+    text-align: center;
+    &:hover {
+        background-color: grey;
+    }
+`;
 
 const StyledTitleInfo = styled.div`
     font-size: 12px;
@@ -60,7 +120,8 @@ const StyledTitleInput = styled.input`
 `;
 
 const StyledQuestionContainer = styled.div`
-    width: 100%;
+    width: 80%;
+    margin: 0 auto;
 `;
 
 export default Question;
