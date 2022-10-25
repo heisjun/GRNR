0834e94cb9f1fb48616688a62e804a6e37dc8f34
heisjun <jun146517@gmail.com>
2022-10-25 16:55:04 +0900

    login test

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -10,7 +10,7 @@ const Login: React.FC = () => {
     const nav = useNavigate();
 
     const [loginStatus, setLoginStatus] = useRecoilState(UserInfo);
-    const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?client_id=${REST_API_KEY}&redirect_uri=${REDIRECT_URI}&response_type=code`;
+    const KAKAO_AUTH_URL = `https://www.gardenersclub.co.kr/api/oauth2/authorization?kakao`;
 
     const handleLogin = () => {
         window.location.href = KAKAO_AUTH_URL;
