4fc335b513b57c1081a39d200aec5c80d686c6a4
Yunseonghwan <sunghwan0208@naver.com>
2023-05-01 01:01:24 +0900

    feat/error

--- src/common/components/FAQ/Faq.tsx
+++ src/common/components/FAQ/Faq.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useState } from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
 import { IFaq } from './Faq.type';
 import * as DOMPurify from 'dompurify';
